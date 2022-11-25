5d3e89d26008e464b9b77a5b20d28059318010a2
Konstantin Voloshchuk <kosv.dev@gmail.com>
2022-11-26 04:16:44 +0600

    Add global_namespace_import rule to cs fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,5 +12,10 @@ return $config->setRules([
     'array_syntax' => ['syntax' => 'short'],
     'ordered_imports' => ['sort_algorithm' => 'alpha'],
     'static_lambda' => true,
+    'global_namespace_import' => [
+        'import_classes' => true,
+        'import_constants' => true,
+        'import_functions' => true,
+    ],
 ])
     ->setFinder($finder);
