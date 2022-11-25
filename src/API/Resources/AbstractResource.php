d8c8a1a93859fe35b08e7c8320f8fb616ac5a0a8
Konstantin Voloshchuk <kosv.dev@gmail.com>
2022-11-26 03:28:24 +0600

    Implement abstract resource

---
+++ src/API/Resources/AbstractResource.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\API\Resources;
+
+use Kosv\DonationalertsClient\Exceptions\ValidateException;
+use Kosv\DonationalertsClient\Validator\ValidationErrors;
+use ReflectionClass;
+use function sprintf;
+
+abstract class AbstractResource
+{
+    /** @var array<string,mixed> */
+    private array $content;
+
+    /**
+     * @param array<string,mixed> $payload
+     */
+    public function __construct(array $payload)
+    {
+        $this->prepare($payload);
+    }
+
+    /**
+     * @return mixed
+     */
+    final protected function getValue(string $key)
+    {
+        return $this->content[$key];
+    }
+
+    abstract protected function getPayloadContentKey(): string;
+
+    /**
+     * @param array<string,mixed> $payload
+     */
+    abstract protected function validate(array $payload): ValidationErrors;
+
+    private function getResourceName(): string
+    {
+        return (new ReflectionClass($this))->getShortName();
+    }
+
+    /**
+     * @param array<string,mixed> $payload
+     * @throws ValidateException
+     */
+    private function prepare(array $payload): void
+    {
+        if (empty($payload[$this->getPayloadContentKey()])) {
+            throw new ValidateException(sprintf(
+                'Payload of %s resource is not valid. Error: payload not contains required content key "%s"',
+                $this->getResourceName(),
+                $this->getPayloadContentKey()
+            ));
+        }
+
+        $errors = $this->validate($payload[$this->getPayloadContentKey()]);
+        if (!$errors->isEmpty()) {
+            $firstError = $errors->getFirstError();
+            throw new ValidateException(sprintf(
+                'Payload of %s resource is not valid. Error: "%s":"%s"',
+                $this->getResourceName(),
+                $firstError->getKey(),
+                $firstError->getError()
+            ));
+        }
+
+        $this->content = $payload[$this->getPayloadContentKey()];
+    }
+}
