368de144435eeb80397ac1bf25298410448dd0c2
Konstantin Voloshchuk <kosv.dev@gmail.com>
2022-12-02 16:41:39 +0600

    Implement RawResourceExtractorTest

---
+++ src/API/RawResourceExtractor.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\API;
+
+use Kosv\DonationalertsClient\Exceptions\ValidateException;
+use Kosv\DonationalertsClient\Validator\KeysEnum;
+use Kosv\DonationalertsClient\Validator\Rules\IsKeyableArrayRule;
+use Kosv\DonationalertsClient\Validator\Rules\IsListableArrayRule;
+use Kosv\DonationalertsClient\Validator\Rules\RequiredFieldRule;
+use Kosv\DonationalertsClient\Validator\Validator;
+use function sprintf;
+
+final class RawResourceExtractor
+{
+    private const DEFAULT_CONTENT_KEY = 'data';
+    private const DEFAULT_METADATA_KEY = 'meta';
+
+    /** @var array<string,mixed> */
+    private array $payload;
+
+    /**
+     * @param array<string,mixed> $payload
+     */
+    public function __construct(array $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    public function extractContent(string $key = self::DEFAULT_CONTENT_KEY, bool $keyable = true): array
+    {
+        return $this->extract($key, [
+            new RequiredFieldRule(KeysEnum::WHOLE_TARGET, [$key]),
+            $keyable ? new IsKeyableArrayRule($key) : new IsListableArrayRule($key),
+        ]);
+    }
+
+    public function extractMetadata(string $key = self::DEFAULT_METADATA_KEY): array
+    {
+        return $this->extract($key, [
+            new RequiredFieldRule(KeysEnum::WHOLE_TARGET, [$key]),
+            new IsKeyableArrayRule($key),
+        ]);
+    }
+
+    private function extract(string $key, array $rules): array
+    {
+        $errors = (new Validator($rules))->validate($this->payload);
+        if (!$errors->isEmpty()) {
+            $firstError = $errors->getFirstError();
+            throw new ValidateException(sprintf(
+                'Payload is not valid. Error: "%s":"%s"',
+                $firstError->getKey(),
+                $firstError->getError()
+            ));
+        }
+
+        return $this->payload[$key];
+    }
+}
---
+++ tests/DonationalertsClient/API/RawResourceExtractorTest.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\Tests\API;
+
+use Kosv\DonationalertsClient\API\RawResourceExtractor;
+use Kosv\DonationalertsClient\Exceptions\ValidateException;
+use PHPUnit\Framework\TestCase;
+
+final class RawResourceExtractorTest extends TestCase
+{
+    public function testExtractContent(): void
+    {
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'data' => [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]))->extractContent(),
+            [
+                'foo' => 1,
+                'bar' => 'val'
+            ]
+        );
+
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'content' => [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]))->extractContent('content'),
+            [
+                'foo' => 1,
+                'bar' => 'val'
+            ]
+        );
+
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'content' => [
+                    [
+                        'foo' => 1,
+                        'bar' => 'val'
+                    ]
+                ]
+            ]))->extractContent('content', false),
+            [
+                [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]
+        );
+    }
+
+    public function testExtractContentWithoutContentKey(): void
+    {
+        $this->expectException(ValidateException::class);
+        $this->expectExceptionMessage('Payload is not valid. Error: "[*]":"Required fields [data] are not set"');
+
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'other' => [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]))->extractContent(),
+            [
+                'foo' => 1,
+                'bar' => 'val'
+            ]
+        );
+    }
+
+    public function testExtractMetadata(): void
+    {
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'meta' => [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]))->extractMetadata(),
+            [
+                'foo' => 1,
+                'bar' => 'val'
+            ]
+        );
+
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'metadata' => [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]))->extractMetadata('metadata'),
+            [
+                'foo' => 1,
+                'bar' => 'val'
+            ]
+        );
+    }
+
+    public function testExtractMetadataWithoutMetadataKey(): void
+    {
+        $this->expectException(ValidateException::class);
+        $this->expectExceptionMessage('Payload is not valid. Error: "[*]":"Required fields [meta] are not set"');
+
+        $this->assertEquals(
+            (new RawResourceExtractor([
+                'other' => [
+                    'foo' => 1,
+                    'bar' => 'val'
+                ]
+            ]))->extractMetadata(),
+            [
+                'foo' => 1,
+                'bar' => 'val'
+            ]
+        );
+    }
+}
