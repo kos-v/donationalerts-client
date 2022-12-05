766e99c22debf40e9cfc78613dcedd28f885a33e
Konstantin Voloshchuk <kosv.dev@gmail.com>
2022-12-05 15:01:39 +0600

    Write IsListableArrayRule unit-test

---
+++ tests/Validator/Rules/IsListableArrayRuleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\Tests\Validator\Rules;
+
+use Kosv\DonationalertsClient\Validator\Rules\IsListableArrayRule;
+use PHPUnit\Framework\TestCase;
+
+final class IsListableArrayRuleTest extends TestCase
+{
+    /**
+     * @dataProvider checkDataProvider
+     */
+    public function testCheck($value, string $expectedMsg): void
+    {
+        $rule = new IsListableArrayRule('test_key', $expectedMsg);
+        $this->assertEquals($rule->check($value), $expectedMsg);
+    }
+
+    public function checkDataProvider(): array
+    {
+        return [
+            [[], ''],
+            [[1], ''],
+            [[1, '', ['key1' => 1]], ''],
+            [null, 'The value must be listable array type'],
+            [1, 'The value must be listable array type'],
+            [['key1' => 1], 'The value must be listable array type'],
+            [['key1' => 1, 2], 'The value must be listable array type'],
+        ];
+    }
+
+    public function testCheckWithoutDefaultErrMessage(): void
+    {
+        $rule = new IsListableArrayRule('test_key', 'Error');
+        $this->assertEquals($rule->check(null), 'Error');
+    }
+}
