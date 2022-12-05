6a586f186b02ce4584e38751ba45ff95b159010d
Konstantin Voloshchuk <kosv.dev@gmail.com>
2022-12-05 14:13:46 +0600

    Write IsKeyableArrayRule unit-test

---
+++ tests/Validator/Rules/IsKeyableArrayRuleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\Tests\Validator\Rules;
+
+use Kosv\DonationalertsClient\Validator\Rules\IsKeyableArrayRule;
+use PHPUnit\Framework\TestCase;
+
+final class IsKeyableArrayRuleTest extends TestCase
+{
+    /**
+     * @dataProvider checkDataProvider
+     */
+    public function testCheck($value, string $expectedMsg): void
+    {
+        $rule = new IsKeyableArrayRule('test_key', $expectedMsg);
+        $this->assertEquals($rule->check($value), $expectedMsg);
+    }
+
+    public function checkDataProvider(): array
+    {
+        return [
+            [[], ''],
+            [['key1' => 1], ''],
+            [['key1' => 1, 'key2' => 2], ''],
+            [null, 'The value must be keyable array type'],
+            [1, 'The value must be keyable array type'],
+            [[[]], 'The value must be keyable array type'],
+            [[1, 2, 3], 'The value must be keyable array type'],
+        ];
+    }
+
+    public function testCheckWithoutDefaultErrMessage(): void
+    {
+        $rule = new IsKeyableArrayRule('test_key', 'Error');
+        $this->assertEquals($rule->check(null), 'Error');
+    }
+}
