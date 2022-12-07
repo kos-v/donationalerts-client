edcde6fd3adc58b059ba795090d8039c9196b418
Konstantin Voloshchuk <kosv.dev@gmail.com>
2022-12-07 22:43:12 +0600

    Write IsLrgOrEqualNumberRule unit-test

---
+++ tests/Validator/Rules/IsLrgOrEqualNumberRuleTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\Tests\Validator\Rules;
+
+use InvalidArgumentException;
+use Kosv\DonationalertsClient\Validator\Rules\IsLrgOrEqualNumberRule;
+use PHPUnit\Framework\TestCase;
+
+final class IsLrgOrEqualNumberRuleTest extends TestCase
+{
+    /**
+     * @dataProvider checkDataProvider
+     */
+    public function testCheck(int $min, $value, string $expectedMsg): void
+    {
+        $rule = new IsLrgOrEqualNumberRule('test_key', $min);
+        $this->assertEquals($expectedMsg, $rule->check($value));
+    }
+
+    public function checkDataProvider(): array
+    {
+        return [
+            [0, 0, ''],
+            [0, 1, ''],
+            [3, 7, ''],
+            [0, -1, 'The value must be larger than or equal to 0'],
+            [3, 2, 'The value must be larger than or equal to 3'],
+        ];
+    }
+
+    public function testCheckWhenValueTypeNotNumeric(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The $value argument must be a numeric type');
+
+        $rule = new IsLrgOrEqualNumberRule('test_key', 0);
+        $rule->check('');
+    }
+
+    public function testCheckWithoutDefaultErrMessage(): void
+    {
+        $rule = new IsLrgOrEqualNumberRule('test_key', 0, false, 'Error {{min}}');
+        $this->assertEquals('Error 0', $rule->check(-1));
+    }
+
+    public function testCheckWhenNullableTrue(): void
+    {
+        $rule = new IsLrgOrEqualNumberRule('test_key', 0, true);
+        $this->assertEquals('', $rule->check(null));
+    }
+}
