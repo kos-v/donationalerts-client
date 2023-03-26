47802246d3d936192258c1c8555261e372210980
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-03-26 15:50:48 +0600

    [Refactor] The API\Actions\V1 class must be inherited from the API\AbstractAction

--- src/API/Actions/V1/V1.php
+++ src/API/Actions/V1/V1.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Kosv\DonationalertsClient\API\Actions\V1;
 
+use Kosv\DonationalertsClient\API\AbstractAction;
 use Kosv\DonationalertsClient\API\Actions\V1\Alerts\Alerts;
 use Kosv\DonationalertsClient\API\Actions\V1\User\User;
 use Kosv\DonationalertsClient\API\Client;
 
-final class V1
+final class V1 extends AbstractAction
 {
     /** @psalm-readonly */
     private Client $client;
