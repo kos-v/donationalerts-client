9cd8882895f0c5449df75742f5fb50d2373def35
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-06-01 00:47:28 +0600

    Implement api's request signer

---
+++ src/API/Signer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\API;
+
+use function array_map;
+use function array_merge;
+use function hash;
+use function implode;
+use function is_array;
+use function sort;
+
+final class Signer
+{
+    /** @psalm-readonly */
+    private string $clientSecret;
+
+    /**
+     * @param non-empty-string $clientSecret
+     */
+    public function __construct(string $clientSecret)
+    {
+        $this->clientSecret = $clientSecret;
+    }
+
+    public function signPayload(AbstractSignablePayload $payload): AbstractSignablePayload
+    {
+        $payloadValues = array_map(
+            static fn($v) => (string)$v,
+            $this->extractPayloadValuesAsList($payload->toFormat())
+        );
+        sort($payloadValues);
+
+        return $payload->setSignature(hash('sha256', implode([
+            ...$payloadValues,
+            $this->clientSecret,
+        ])));
+    }
+
+    private function extractPayloadValuesAsList(array $payload): array
+    {
+        $values = [];
+        foreach ($payload as $value) {
+            if (is_array($value)) {
+                $values = array_merge($values, $this->extractPayloadValuesAsList($value));
+            } else {
+                $values[] = $value;
+            }
+        }
+
+        return $values;
+    }
+}
---
+++ tests/API/SignerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\Tests\API;
+
+use Kosv\DonationalertsClient\API\AbstractSignablePayload;
+use Kosv\DonationalertsClient\API\Signer;
+use Kosv\DonationalertsClient\Validator\ValidationErrors;
+use PHPUnit\Framework\TestCase;
+
+final class SignerTest extends TestCase
+{
+    private const PAYLOAD_SIGNATURE_FIELD_KEY = 'signature';
+
+    /**
+     * @dataProvider getSignPayloadDataProvider
+     */
+    public function testSignPayload(string $secret, AbstractSignablePayload $payload, string $expectedSignature): void
+    {
+        $signer = new Signer($secret);
+        $this->assertEquals(
+            $expectedSignature,
+            $signer->signPayload($payload)->toFormat()[self::PAYLOAD_SIGNATURE_FIELD_KEY]
+        );
+    }
+
+    public function getSignPayloadDataProvider(): iterable
+    {
+        return [
+            [
+                '',
+                $this->makeSignablePayload([]),
+                'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+            ],
+            [
+                'secret',
+                $this->makeSignablePayload([]),
+                '2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b'
+            ],
+            [
+                'secret',
+                $this->makeSignablePayload([
+                    'a' => null,
+                    'b' => -1,
+                    'c' => 0,
+                    'd' => 1,
+                    'e' => '',
+                    'f' => 'string',
+                    'g' => [],
+                    'h' => [3, 'String', null],
+                    'i' => [
+                        'a' => 1234567890,
+                        'b' => 'strInG'
+                    ],
+                    'j' => '123string'
+                ]),
+                '2de86dbe790a9d36cbdd327ef6de898d722def50cbcad57cb9d463b21b360890'
+            ]
+        ];
+    }
+
+    private function makeSignablePayload(array $payload): AbstractSignablePayload
+    {
+        return new class ($payload, self::PAYLOAD_SIGNATURE_FIELD_KEY) extends AbstractSignablePayload {
+            private string $signatureFieldKey;
+
+            public function __construct(array $payload, string $signatureFieldKey)
+            {
+                $this->signatureFieldKey = $signatureFieldKey;
+                parent::__construct($payload, AbstractSignablePayload::FORMAT_POST_FIELDS);
+            }
+
+            public function getSignatureFieldKey(): string
+            {
+                return $this->signatureFieldKey;
+            }
+
+            protected function validatePayload($payload): ValidationErrors
+            {
+                return new ValidationErrors();
+            }
+        };
+    }
+}
