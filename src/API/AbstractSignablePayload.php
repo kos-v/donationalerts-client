61b3a33b06eac6f201b0a5c92a74d51a6987d074
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-05-31 20:58:08 +0600

    Implement AbstractSignablePayload class for signable payloads

---
+++ src/API/AbstractSignablePayload.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosv\DonationalertsClient\API;
+
+abstract class AbstractSignablePayload extends AbstractPayload
+{
+    /**
+     * @return $this
+     */
+    final public function setSignature(string $signature)
+    {
+        $this->setValueToArrayPayload($this->getSignatureFieldKey(), $signature);
+        return $this;
+    }
+
+    abstract protected function getSignatureFieldKey(): string;
+}
